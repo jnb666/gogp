21bea6a75aa1e0d73d89d78be4c5bbc60d563db5
John Banks <jnb666@gmail.com>
2013-12-25 23:20:48 +0000

    updates to stats interface + add tests

--- stats/stats.go
+++ stats/stats.go
@@ -5,6 +5,7 @@ import (
     "math"
     "reflect"
     "strings"
+    "encoding/json"
     "github.com/jnb666/gogp/gp"
 )
 
@@ -17,61 +18,127 @@ var (
     LogColumnWidth = 8
 )
 
-// Stats structure holds the statistics for the give Population. 
+// Stats structure holds the statistics for the give Population.
+// Struct tags are used for descriptive field name in JSON encoding
 type Stats struct {
-    Gen     int
-    Evals   int
-    Best    *gp.Individual
-    Fit     StatsData
-    Size    StatsData
-    Depth   StatsData
+    Gen     int             `desc:"generation"`
+    Evals   int             `desc:"no. of evals"`
+    Best    *gp.Individual  `desc:"best individual"`
+    Fit     *StatsData      `desc:"fitness"`
+    Size    *StatsData      `desc:"size"`
+    Depth   *StatsData      `desc:"depth"`
 }
 
-// Stats data holds the values for a single metric
-type StatsData struct { Min, Max, Avg, Std float64 }
+// Stats data holds the values for a single metric.
+type StatsData struct { 
+    Min  float64    `desc:"minimum"`
+    Max  float64    `desc:"maximum"`
+    Avg  float64    `desc:"mean"`
+    Std  float64    `desc:"std deviation"`
+}
+
+// StatsHistory slice stores all the stats for a given run
+type StatsHistory []*Stats
+
+// PlotData struct is used for encoding the StatsHistory to JSON e.g. for plotting
+type PlotData struct {
+    Label string        `json:"label"`
+    Data  [][2]float64  `json:"data"`
+}
 
 // GetStats calculates stats on fitness, size and depth for the given population
 func Create(pop gp.Population, gen, evals int) *Stats {
-    s := &Stats{ Gen:gen, Evals:evals }
-    updateStats(pop, &s.Fit, func(ind *gp.Individual)float64 { return ind.Fitness })
-    updateStats(pop, &s.Size, func(ind *gp.Individual)float64 { return float64(ind.Size()) })
-    updateStats(pop, &s.Depth, func(ind *gp.Individual)float64 { return float64(ind.Depth()) })
+    s := &Stats{ Gen:gen, Evals:evals, Fit:NewStatsData(), Size:NewStatsData(), Depth:NewStatsData() }
+    updateStats(pop, s.Fit, func(ind *gp.Individual)float64 { return ind.Fitness })
+    updateStats(pop, s.Size, func(ind *gp.Individual)float64 { return float64(ind.Size()) })
+    updateStats(pop, s.Depth, func(ind *gp.Individual)float64 { return float64(ind.Depth()) })
     s.Best = pop.Best().Clone()
     return s
 }
 
-// update stats data
+// NewStatsData initialises a new StatsData struct
+func NewStatsData() *StatsData {
+    return &StatsData{ Min: 1e99, Max: 1e-99 }
+}
+
+// update stats data, calc running mean and variance
 func updateStats(pop gp.Population, d *StatsData, getval func(*gp.Individual)float64) {
-    psize := float64(len(pop))
-    d.Min, d.Max = 1e99, -1e99
-    for _, ind := range pop {
+    var oldM, oldS float64
+    for i, ind := range pop {
         val := getval(ind)
         if val > d.Max { d.Max = val }
         if val < d.Min { d.Min = val }
-        d.Avg += val / psize
+        if i == 0 {
+            oldM, d.Avg = val, val
+        } else {
+            d.Avg = oldM + (val-oldM)/float64(i+1)
+            d.Std = oldS + (val-oldM)*(val-d.Avg)
+            oldM, oldS = d.Avg, d.Std
+        }
     }
-    for _, ind := range pop {
-        val := getval(ind)
-        d.Std += (val-d.Avg)*(val-d.Avg) / psize
+    if len(pop) > 1 {
+        d.Std = math.Sqrt(d.Std / float64(len(pop)-1))
     }
-    d.Std = math.Sqrt(d.Std)
 }
 
-// The Get method returns the data in the named field, name must be a valid field else this will panic
-func (d *StatsData) Get(name string) interface{} {
-    fld := reflect.ValueOf(d).Elem().FieldByName(name)
-    return fld.Interface()
+// get struct field and desc tag by reflection
+func getField(struc reflect.Value, name string) (val interface{}, tag string, err error) {
+    fld := struc.Elem().FieldByName(name)
+    if !fld.IsValid() {
+        err = fmt.Errorf("Stats field %s is not valid", name)
+        return
+    }
+    val = fld.Interface()
+    if tfld, ok := struc.Elem().Type().FieldByName(name); ok {
+        tag = tfld.Tag.Get("desc")
+    }
+    return
 }
 
-// The Get method returns the data in the named field. If dot notation is used then it will
-// extract the subfield from the StatsData struct
-func (s *Stats) Get(name string) (val interface{}) {
-    el := reflect.ValueOf(s).Elem()
-    if fields := strings.Split(name, "."); len(fields) > 1 {
-        data := el.FieldByName(fields[0]).Interface().(StatsData)
-        return data.Get(fields[1])
+// The Get method returns the data and desc tag for the named StatsData field,
+// or an error if the field does not exist.
+func (d *StatsData) Get(name string) (val interface{}, tag string, err error) {
+    return getField(reflect.ValueOf(d), name)
+}
+
+// The Get method returns the data and desc tag for the named field or an error if field does not exist. 
+// If dot notation is used then it will extract the subfield from the StatsData struct.
+func (s *Stats) Get(name string) (val interface{}, tag string, err error) {
+    names := strings.Split(name, ".")
+    if val, tag, err = getField(reflect.ValueOf(s), names[0]); err != nil {
+        return
     }
-    return el.FieldByName(name).Interface()
+    if len(names) > 1 {
+        val, tag, err = val.(*StatsData).Get(names[1])
+    }
+    return
+}
+
+// The Get method returns the history data for the named field in a suitable format for plotting
+// Returns FieldNotFound error if name is not a valid field
+func (h StatsHistory) Get(name string) (p PlotData, err error) {
+    var val interface{}
+    p.Data  = make([][2]float64, len(h))
+    p.Label = name
+    for i, stats := range h {
+        if val, p.Label, err = stats.Get(name); err != nil {
+            return
+        }
+        if fval, ok := val.(float64); ok { 
+            p.Data[i] = [2]float64{ float64(i), fval }
+        } else {
+            err = fmt.Errorf("Stats field %s could not be converted to float", name)
+        }
+    }
+    return
+}
+
+// The GetJSON method calls Get to retrieve the data, then encodes in JSON format for plotting.
+// name provided should be the name of a StatsData struct (Fit / Size / Depth)
+func (h StatsHistory) GetJSON(name string) ([]byte, error) {
+    data, err := h.Get(name)
+    if err != nil { return nil, err }
+    return json.Marshal(data)
 }
 
 // String method returns formatted stats data for logging
@@ -88,7 +155,8 @@ func (s *Stats) String() string {
     }
     for i, col := range LogColumn {
         format := fmt.Sprintf("%%-%d%s", LogColumnWidth, LogColumnFmt[i])
-        cols[i] = fmt.Sprintf(format, s.Get(col))
+        val, _, _ := s.Get(col)
+        cols[i] = fmt.Sprintf(format, val)
     }
     // testing package does not like trailing space in examples!
     text += strings.TrimSpace(strings.Join(cols, " "))
---
+++ stats/stats_test.go
@@ -0,0 +1,65 @@
+package stats
+import (
+	"testing"
+    "math/rand"
+    "fmt"
+    "github.com/jnb666/gogp/gp"
+    "github.com/jnb666/gogp/num"
+)
+
+var exp = `Gen      Evals    FitMax   FitAvg   FitStd   SizeAvg  SizeMax  DepthAvg DepthMax
+0        1000     1        0.493    0.285    6.74     15       1.94     3`
+
+var fields = []string{"Gen", "Evals", "Fit.Max", "Fit.Avg", "Fit.Std", "Size.Max", "Depth.Max" }
+
+func getStats(t *testing.T, gen int) *Stats {
+    pset := gp.CreatePrimSet(1, "x")
+    pset.Add(num.Add, num.Sub, num.Mul, num.Div, num.Neg, num.V(0), num.V(1))
+    pop := gp.CreatePopulation(1000, gp.GenFull(pset, 1, 3))
+    for i := range pop {
+        pop[i].Fitness = rand.Float64()
+    }
+    s := Create(pop, gen, len(pop))
+    t.Log(s)
+    return s
+}
+
+// test generating population and calculating stats
+func TestStats(t *testing.T) {
+    gp.SetSeed(1)
+    s := getStats(t, 0)
+    if fmt.Sprint(s) != exp {
+        t.Error("stats text looks wrong! Expected\n", exp)
+    }
+    for _, fld := range fields {
+        val, tag, err := s.Get(fld)
+        if err != nil { t.Error(err) }
+        t.Log(fld, "=>" , tag, val)
+    }
+    _, _, err := s.Get("Fit.Foo")
+    if fmt.Sprint(err) != "Stats field Foo is not valid" { 
+        t.Error("expected error for missing field")
+    }
+}
+
+var jsonExp = `{"label":"maximum","data":[[0,15],[1,15],[2,15],[3,15],[4,15]]}`
+
+// test json data for plotting
+func TestJson(t *testing.T) {
+    gp.SetSeed(1)
+    history := make(StatsHistory, 5)
+    for i := range history {
+        history[i] = getStats(t, i)
+    }
+    data, err := history.GetJSON("Size.Max")
+    if err != nil { t.Error(err) }
+    t.Log(string(data))
+    if string(data) != jsonExp {
+        t.Error("json data looks wrong! Expected\n", jsonExp)
+    }
+    data, err = history.GetJSON("Foo")
+    if fmt.Sprint(err) != "Stats field Foo is not valid" { 
+        t.Error("expected error for missing field")
+    }
+}
+
